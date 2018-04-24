8700f02d086875e67faa5d17abba25e416c3dc04
Vipul Sharma <vipuls526@gmail.com>
2018-04-24 03:25:59 -0700

    Update chain_reaction.cpp

--- game_1.0/chain_reaction.cpp
+++ game_1.0/chain_reaction.cpp
@@ -11,9 +11,11 @@ class ChainReaction{
 		void createGame();
 		void printRawGame();
 		void printGame();
+		void printRawNodeCount();
 		void play();
 		void markIndex(PNodeptr, int);
 		int gameLeft();
+		int playerLeft();
 };
 
 ChainReaction::ChainReaction(int gameSize, int playerCount){
@@ -39,7 +41,9 @@ void ChainReaction::createGame(){
 			}else{
 				int uindex = ((index/this->gameSize-1)*this->gameSize + index%this->gameSize);
 				node->up = this->gmap[uindex];
+				node->total++;
 				node->up->down = node;
+				node->up->total++;
 			}
 			
 			if(colnum==0){
@@ -47,13 +51,17 @@ void ChainReaction::createGame(){
 			}else{
 				int lindex = index-1;
 				node->left = this->gmap[lindex];
+				node->total++;
 				node->left->right = node;
+				node->left->total++;
 			}
 		}
 	}
 }
 
 void ChainReaction::play(){
+	
+	//get the first moves to initialize the game
 	PNodeptr pnode = this->firstPlayer;
 	do{
 		this->printGame();
@@ -66,19 +74,34 @@ void ChainReaction::play(){
 		this->markIndex(pnode, index);
 		pnode = pnode->next;
 	}while(pnode!=this->firstPlayer);
-	//while(this->gameLeft()){
-		
-	//}
-	this->printGame();
+	
+	//the game has already begun!!!
+	do{
+		if(this->playerLeft()){
+			this->printGame();
+			cout<<"Player "<<getPlayerName(pnode->player)<<" please enter your index: ";
+			int index;
+			cin>>index;
+			while(index>=this->gameSize*this->gameSize || this->gmap[index]->count == this->gmap[index]->total || this->gmap[index]->player!=pnode){
+				cout<<"Please choose a valid index: "; cin>>index;
+			}
+			this->markIndex(pnode, index);
+			pnode = pnode->next;
+		}else{
+			
+		}
+	}while(this->gameLeft());
+	cout<<"Hurrah! "<<getPlayerName(pnode->prev->player)<<" has won the game."<<endl;
+	//this->printGame();
 }
 
-/*
+
 int ChainReaction::gameLeft(){
-	for(int i=0; i<this->gameSize*this->gameSize; i++){
-		if(this->)
+	for(int index=0; index<this->gameSize*this->gameSize; index++){
+		if(this->gmap[index]->count<this->gmap[index]->total) return 1;
 	}
+	return 0;
 }
-*/
 
 
 void ChainReaction::markIndex(PNodeptr pnode, int index){
@@ -124,6 +147,25 @@ void ChainReaction::printRawGame(){
 			int index = rownum*gameSize+colnum;
 			if(index/10==0) cout<<"0";
 			cout<<index<<"|";
+		}
+		cout<<endl;
+	}
+	for(int i=0; i<(this->gameSize*2 + this->gameSize+1); i++){
+		cout<<"-";
+	}
+	cout<<endl;
+}
+
+void ChainReaction::printRawNodeCount(){
+	for(int i=0; i<(this->gameSize*2 + this->gameSize+1); i++){
+		cout<<"-";
+	}
+	cout<<endl;
+	for(int rownum=0; rownum<this->gameSize; rownum++){
+		cout<<"|";
+		for(int colnum=0; colnum<this->gameSize; colnum++){
+			int index = rownum*gameSize+colnum;
+			cout<<"0"<<this->gmap[index]->total<<"|";
 		}
 		cout<<endl;
 	}
